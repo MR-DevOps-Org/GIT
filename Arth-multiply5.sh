3c2c0c8e112cd4b12794dab1ca9fef3d8cf92afe
Ranjeeth28 <ranjeeth.m6@gmail.com>
2020-06-07 01:45:37 +0530

    Deleted file

--- Arth-multiply5.sh
+++
@@ -1,8 +0,0 @@
-echo "Give a number to get multipliers of it"
-echo "The number is : "
-read num
-for (a = 1; a<=10; a++ )
-	echo "Here is the Multiplers of $a"
-do "$num * $a " = $num * $a
-done
-	
