8c54fbfef7c8ff579801c264b7213b1fb0154e3c
Ranjeeth28 <ranjeeth.m6@gmail.com>
2020-06-06 13:12:54 +0530

    first commit

---
+++ Arth-multiply5.sh
@@ -0,0 +1,8 @@
+echo "Give a number to get multipliers of it"
+echo "The number is : "
+read num
+for (a = 1; a<=10; a++ )
+	echo "Here is the Multiplers of $a"
+do "$num * $a " = $num * $a
+done
+	
---
+++ shell.sh
@@ -0,0 +1,1 @@
+echo " Hello .. Juz created file from GIT"
